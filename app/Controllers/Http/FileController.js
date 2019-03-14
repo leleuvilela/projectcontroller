03b13c79a13cd11581fdd361563de604b61953a5
Vinícius Vilela <viniciusvilela19@gmail.com>
2019-03-14 18:18:12 -0300

    primeiro commit

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const File = use('App/Models/File')
+const Helpers = use('Helpers')
+
+class FileController {
+  async show ({ params, response }) {
+    const file = await File.findOrFail(params.id)
+
+    return response.download(Helpers.tmpPath(`uploads/${file.file}`))
+  }
+
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '2mb' })
+
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), { name: fileName })
+
+      if (!upload.moved()) {
+        let message
+        if (upload.error().type == 'size') message = 'Arquivo muito grande'
+        else message = 'Erro ao processar arquivo'
+        return response.status(400).send({ error: { message } })
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Erro no upload de arquivo' } })
+    }
+  }
+}
+
+module.exports = FileController
